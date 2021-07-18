191b8c48380b280be7ac1921ceafaeec6cc5fb3d
frzhayder <84817253+frzhayder@users.noreply.github.com>
2021-07-18 18:02:27 +0200

    Add files via upload

---
+++ Coding Exercise 2.py
@@ -0,0 +1,15 @@
+def division(a,b):
+    # we use if and else statement 
+    if b == 0:
+        print('Error: You cannot divide by 0, please choose an appropriate denominator.');
+        # as if denominator is zero, it will print  Error. 
+    else:
+        division= a / b;
+        #we will use division
+
+        print('my first number {0} / my 2nd number {1} = {2}.' .format(a,b,division));
+        #print('my frist number divided by my second number =',division);
+        #print('my first number is %s' and 'my second number is %d', %(division))
+        
+              
+        
